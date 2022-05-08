38d5e9cf9abb8e46e403d094f8e4053486abb3fb
Yunis Huseynzade <yunisdev.04@gmail.com>
2022-05-08 12:18:27 +0400

    Type declaration fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Button from './components/basics/Button'
 import Input from './components/forms/Input'
 import ContactlessProvider from './components/misc/ContactlessProvider'
-import Container from './components/layout/Container';
-import Block from './components/layout/Block';
-export { Button, Input, ContactlessProvider, Container, Block }
+import Container from './components/layout/Container'
+import Block from './components/layout/Block'
+import Navbar, { NavLink } from './components/basics/Navbar'
+export { Button, Input, ContactlessProvider, Container, Block, Navbar, NavLink }
