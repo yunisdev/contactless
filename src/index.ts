97be868ccbed257ca7645111967d40e63b2cf38b
Yunis Huseynzade <yunisdev.04@gmail.com>
2022-05-08 13:38:54 +0400

    createTheme export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,14 @@ import ContactlessProvider from './components/misc/ContactlessProvider'
 import Container from './components/layout/Container'
 import Block from './components/layout/Block'
 import Navbar, { NavLink } from './components/basics/Navbar'
-export { Button, Input, ContactlessProvider, Container, Block, Navbar, NavLink }
+import { createTheme } from './theming'
+export {
+	Button,
+	Input,
+	ContactlessProvider,
+	Container,
+	Block,
+	Navbar,
+	NavLink,
+	createTheme,
+}
